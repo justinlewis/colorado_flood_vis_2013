87af83fb43c4b94a5f8da2b5f3d27d3f309a97c0
justinlewis <j.mapping@gmail.com>
2013-09-15 14:01:47 -0600

    added map and more charts

---
+++ station_points.js
@@ -0,0 +1,14 @@
+var points = {
+"type": "FeatureCollection",
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "gid": 1, "sitename": "Boulder Creek @ Longmont", "type": "gauge", "river": "Boulder Creek", "maxflow": "5170" }, "geometry": { "type": "Point", "coordinates": [ -105.015094, 40.152039 ] } }
+,
+{ "type": "Feature", "properties": { "gid": 2, "sitename": "Clear Creek @ Golden", "type": "gauge", "river": "Clear Creek", "maxflow": "6.83"  }, "geometry": { "type": "Point", "coordinates": [ -105.235281, 39.752807 ] } }
+,
+{ "type": "Feature", "properties": { "gid": 2, "sitename": "Bear Creek @ Evergreen", "type": "gauge", "river": "Bear Creek", "maxflow": "1360"  }, "geometry": { "type": "Point", "coordinates": [ -105.337019, 39.633151 ] } }
+
+]
+}
+
+
