7157e7249a7dd58f0530b9b70579d82678735a5a
justinlewis <j.mapping@gmail.com>
2013-09-15 18:47:52 -0600

    small enhancements, data update, data error fix in clear creek

--- station_points.js
+++ station_points.js
@@ -2,19 +2,19 @@ var points = {
 "type": "FeatureCollection",
                                                                                 
 "features": [
-{ "type": "Feature", "properties": { "gid": 6, "sitename": "Big Thompson @ Loveland", "type": "gauge", "river": "Big Thompson", "maxflow": "4670", "gaugestatus": "DAMAGED DURING FLOOD"  }, "geometry": { "type": "Point", "coordinates": [ -105.060564, 40.378688 ] } }
+{ "type": "Feature", "properties": { "gid": 6, "sitename": "Big Thompson @ Loveland", "type": "gauge", "river": "Big Thompson", "maxflow": "4670", "average": "00", "gaugestatus": "DAMAGED DURING FLOOD"  }, "geometry": { "type": "Point", "coordinates": [ -105.060564, 40.378688 ] } }
 ,
-{ "type": "Feature", "properties": { "gid": 1, "sitename": "Boulder Creek @ Longmont", "type": "gauge", "river": "Boulder Creek", "maxflow": "5170", "gaugestatus": "Active" }, "geometry": { "type": "Point", "coordinates": [ -105.015094, 40.152039 ] } }
+{ "type": "Feature", "properties": { "gid": 1, "sitename": "Boulder Creek @ Longmont", "type": "gauge", "river": "Boulder Creek", "maxflow": "5170", "average": "26", "gaugestatus": "Active" }, "geometry": { "type": "Point", "coordinates": [ -105.015094, 40.152039 ] } }
 ,
-{ "type": "Feature", "properties": { "gid": 7, "sitename": "St. Vrain @ Longmont", "type": "gauge", "river": "St. Vrain", "maxflow": "2620", "gaugestatus": "DAMAGED DURING FLOOD"  }, "geometry": { "type": "Point", "coordinates": [ -105.014451, 40.157639 ] } }
+{ "type": "Feature", "properties": { "gid": 7, "sitename": "St. Vrain @ Longmont", "type": "gauge", "river": "St. Vrain", "maxflow": "2620", "average": "00", "gaugestatus": "DAMAGED DURING FLOOD"  }, "geometry": { "type": "Point", "coordinates": [ -105.014451, 40.157639 ] } }
 ,
-{ "type": "Feature", "properties": { "gid": 2, "sitename": "Clear Creek @ Golden", "type": "gauge", "river": "Clear Creek", "maxflow": "6.83", "gaugestatus": "Active"  }, "geometry": { "type": "Point", "coordinates": [ -105.235281, 39.752807 ] } }
+{ "type": "Feature", "properties": { "gid": 2, "sitename": "Clear Creek @ Golden", "type": "gauge", "river": "Clear Creek", "maxflow": "1530", "average": "00", "gaugestatus": "Active"  }, "geometry": { "type": "Point", "coordinates": [ -105.235281, 39.752807 ] } }
 ,
-{ "type": "Feature", "properties": { "gid": 3, "sitename": "Bear Creek @ Evergreen", "type": "gauge", "river": "Bear Creek", "maxflow": "1360", "gaugestatus": "Active"  }, "geometry": { "type": "Point", "coordinates": [ -105.337019, 39.633151 ] } }
+{ "type": "Feature", "properties": { "gid": 3, "sitename": "Bear Creek @ Evergreen", "type": "gauge", "river": "Bear Creek", "maxflow": "1360", "average": "00", "gaugestatus": "Active"  }, "geometry": { "type": "Point", "coordinates": [ -105.337019, 39.633151 ] } }
 ,
-{ "type": "Feature", "properties": { "gid": 4, "sitename": "South Platte @ Fort Lupton", "type": "gauge", "river": "South Platte", "maxflow": "9320", "gaugestatus": "Active"  }, "geometry": { "type": "Point", "coordinates": [ -104.818908, 40.11589 ] } }
+{ "type": "Feature", "properties": { "gid": 4, "sitename": "South Platte @ Fort Lupton", "type": "gauge", "river": "South Platte", "maxflow": "9320", "average": "00", "gaugestatus": "Active"  }, "geometry": { "type": "Point", "coordinates": [ -104.818908, 40.11589 ] } }
 ,
-{ "type": "Feature", "properties": { "gid": 5, "sitename": "Big Thompson @ Estes Park", "type": "gauge", "river": "Big Thompson", "maxflow": "1080", "gaugestatus": "Active"  }, "geometry": { "type": "Point", "coordinates": [ -105.584215, 40.354017 ] } }
+{ "type": "Feature", "properties": { "gid": 5, "sitename": "Big Thompson @ Estes Park", "type": "gauge", "river": "Big Thompson", "maxflow": "1080", "average": "00", "gaugestatus": "Active"  }, "geometry": { "type": "Point", "coordinates": [ -105.584215, 40.354017 ] } }
 
 ]
 }
